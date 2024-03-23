3785b822514d4ff156992698807ea30d9eef10c8
abey546 <abeyhassan546@gmail.com>
2024-03-23 07:55:05 -0700

    install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,11 @@
-# Install flask from pip3
-package { 'flask':
-  ensure   => '2.1.0',
+# install flask from pip3
+package { 'flask' :
+    ensure   => '2.1.0',
+    provider => 'pip3',
+    }
+
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3',
-}
-#install Werkzeug from pip3
-package { 'Werkzeug':
-ensure => '2.1.1',
-provider => 'pip3',
 }
